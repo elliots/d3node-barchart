f3f2977c44267ffc7fecf5b58b99044190555c97
Elliot Shepherd <elliot@upowr.com.au>
2023-01-23 11:47:18 +1100

    remove d3-node-output dependency
    update example to use svg2img

--- example/index.js
+++ example/index.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
-const output = require('d3node-output');
+// const output = require('d3node-output');
+const svg2img = require('svg2img');
 const d3 = require('d3-node')().d3;
 const d3nBar = require('../');
 
 const csvString = fs.readFileSync('data/data.csv').toString();
 const data = d3.csvParse(csvString);
 
-// create output files
-output('./example/output', d3nBar({ data: data }));
+// create output file
+
+const bar = d3nBar({ data: data })
+
+svg2img(bar.svgString(), function(error, buffer) {
+    fs.writeFileSync('example/output.png', buffer);
+});
+
+fs.writeFileSync('example/output.svg', bar.svgString());
+
+fs.writeFileSync('example/output.html', bar.html());
